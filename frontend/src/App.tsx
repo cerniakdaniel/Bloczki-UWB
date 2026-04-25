bcb490015ddbb63a1c7ce50302e8e50980042eac
cerniakdaniel <cerniakdaniel@gmail.com>
2026-04-25 15:06:54 +0300

    Inicjalizacja projektu React + Vite + TypeScript

---
+++ frontend/src/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function App() {
+  return (
+    <div>
+      <h1>Bloczki UWB</h1>
+      <p>System tworzenia schematów blokowych algorytmów</p>
+    </div>
+  );
+}
